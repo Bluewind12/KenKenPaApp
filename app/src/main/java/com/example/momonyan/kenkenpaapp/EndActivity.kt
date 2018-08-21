a19000979754d85bf01f86a85da5e4bb6f7e4bc9
BlueWind <zero18030@gmail.com>
2018-08-21 22:58:40 +0900

    # 終了画面の作成（簡易）

--- app/src/main/java/com/example/momonyan/kenkenpaapp/EndActivity.kt
+++ app/src/main/java/com/example/momonyan/kenkenpaapp/EndActivity.kt
@@ -1,7 +1,40 @@
 package com.example.momonyan.kenkenpaapp
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 
-class EndActivity:AppCompatActivity() {
+class EndActivity : AppCompatActivity() {
+    private lateinit var scoreText: TextView
+    private lateinit var backButton: Button
+    private var score: Long = 0
+    private var scoreMin: Long = 0
+   private lateinit var data :SharedPreferences
+   private lateinit var editor:SharedPreferences.Editor
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+        setScore()
+        scoreText.text = getString(R.string.score,score)
+        backButton.setOnClickListener {
 
+        }
+    }
+
+    private fun init() {
+        scoreText = findViewById(R.id.scoreText)
+        backButton = findViewById(R.id.homeBackButton)
+        score =(intent.getLongExtra("endTime",0) - intent.getLongExtra("time",999999))/1000
+        data = getSharedPreferences("ScoreTable", Context.MODE_PRIVATE)
+        editor = data.edit()
+   }
+    private fun setScore(){
+        if(score < data.getLong("score", Long.MAX_VALUE)){
+            editor.putLong("score",score)
+            editor.apply()
+        }
+    }
 }
