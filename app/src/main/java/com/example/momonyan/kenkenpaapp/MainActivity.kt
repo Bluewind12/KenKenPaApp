e9c2ad75df9dac764019ebdec8869f32e20c7631
BlueWind <zero18030@gmail.com>
2018-08-21 20:33:01 +0900

    # タイトル画面の作成

--- app/src/main/java/com/example/momonyan/kenkenpaapp/MainActivity.kt
+++ app/src/main/java/com/example/momonyan/kenkenpaapp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.momonyan.kenkenpaapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startButton: Button
+    private lateinit var highScoreText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    private fun init() {
+        startButton = findViewById(R.id.startButton1)
+        highScoreText = findViewById(R.id.highScoreTextView)
     }
 }
